3f86c63ddef146af976604ba7e65f126659528ff
maciekto <ekstar007@gmail.com>
2023-10-23 13:44:17 +0200

    Add firebase config to gitignore

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -1,18 +1,9 @@
 import { initializeApp } from 'firebase/app'; // no compat for new SDK
 import { getDatabase, ref } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
+import { firebaseConfig } from './firebaseConfig';
 
 // Get your config here
-const firebaseConfig = {
-  apiKey: "AIzaSyD4U3KSH6xALT0tOsGXJ-a9m9Ud4-Q-sAE",
-  authDomain: "portfoliosettlements.firebaseapp.com",
-  databaseURL: "https://portfoliosettlements-default-rtdb.firebaseio.com",
-  projectId: "portfoliosettlements",
-  storageBucket: "portfoliosettlements.appspot.com",
-  messagingSenderId: "476678319901",
-  appId: "1:476678319901:web:6956f285d3fcd21a937bf4",
-  measurementId: "G-5LS5T94WQZ"
-};
 
 const firebaseApp = initializeApp(firebaseConfig);
 
---
+++ src/services/firebase/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+	apiKey: 'AIzaSyD4U3KSH6xALT0tOsGXJ-a9m9Ud4-Q-sAE',
+	authDomain: 'portfoliosettlements.firebaseapp.com',
+	databaseURL: 'https://portfoliosettlements-default-rtdb.firebaseio.com',
+	projectId: 'portfoliosettlements',
+	storageBucket: 'portfoliosettlements.appspot.com',
+	messagingSenderId: '476678319901',
+	appId: '1:476678319901:web:6956f285d3fcd21a937bf4',
+	measurementId: 'G-5LS5T94WQZ',
+};
